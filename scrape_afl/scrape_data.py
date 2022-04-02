e68eed108b35bec9d4c12af7c08d4e7a47e71624
James Fricker <jimmyf98@hotmail.com>
2022-04-02 22:20:29 +1100

    refactor to main function

--- scrape_afl/scrape_data.py
+++ scrape_afl/scrape_data.py
@@ -5,45 +5,20 @@
 # player details
 # match result?
 
-#https://www.afl.com.au/fixture?Competition=1&CompSeason=4&MatchTimezone=MY_TIME&Regions=2&ShowBettingOdds=1&GameWeeks=14&Teams=1&Venues=12
-
-# https://www.afl.com.au/fixture?Competition=2&CompSeason=43&MatchTimezone=MY_TIME&Regions=2&GameWeeks=3
-
 from bs4 import BeautifulSoup
 import requests
 import time
 from selenium import webdriver
 import pytest
 
-COMPETITION = 1
-COMP_SEASON = 43
-GAMEWEEK = 3
-
-
-
-url = "https://www.afl.com.au/fixture?Competition=%s&CompSeason=%s&MatchTimezone=MY_TIME&Regions=2&GameWeeks=%s&Teams=1&Venues=12" % (COMPETITION,COMP_SEASON, GAMEWEEK)
-
-def get_soup_data(url,sleep = True):
+def get_soup_data(url):
     browser = webdriver.Firefox()
 
     browser.get(url)
-    if sleep == True:
-        time.sleep(5)
     html = browser.page_source
     soup = BeautifulSoup(html, 'html.parser')
     browser.close()
     return soup
-
-def get_all_div(soup):
-    divs = []
-    dates = []
-    for child in soup.recursiveChildGenerator():
-        if child.name:
-            class_name = child.get('class')
-        #print(child.name, class_name)
-        if "match-results" in class_name or "match-list__group-date" in class_name:
-            divs.append(child)
-    return divs
 
 def get_all_links_from_afl_homepage(homepage_soup):
     relevant_links = []
@@ -61,13 +36,21 @@ def get_all_links_from_afl_homepage(homepage_soup):
 def get_info_from_match_soup(soup):
     home_team_score = -1
     away_team_score = -1 
+    home_team_name = 'NA'
+    away_team_name = 'NA'
+    match_date = 'NA'
+    home_team_goals = -1
+    home_team_behinds = -1
+    away_team_goals = -1
+    away_team_behinds = -1
+
     # get team names
     match_title = soup.find('div', class_='mc-header__round-wrapper').text
     match_title = match_title.replace('\n','')
 
     match_title_split = match_title.split('•')
     match_round = match_title_split[0].strip()
-    
+
     team_names = match_title_split[1].split('v')
     home_team_name = team_names[0].strip()
     away_team_name = team_names[1].strip()
@@ -78,6 +61,14 @@ def get_info_from_match_soup(soup):
         home_team_score = scores[0].text
         away_team_score = scores[1].text
 
+    detailed_scores = soup.find_all('span',attrs={'class': 'mc-header__score-split'})
+    if len(detailed_scores) == 2:
+
+        home_team_goals = detailed_scores[0].text.split('.')[0].strip()
+        home_team_behinds = detailed_scores[0].text.split('.')[1].strip()
+        away_team_goals = detailed_scores[1].text.split('.')[0].strip()
+        away_team_behinds = detailed_scores[1].text.split('.')[1].strip()
+    
     # get the date
     match_date = soup.find('div', class_='mc-header__date-wrapper js-match-start-time').text
 
@@ -85,7 +76,11 @@ def get_info_from_match_soup(soup):
                 "home_team_name": home_team_name,
                 "away_team_name": away_team_name,
                 "home_team_score": home_team_score,
+                "home_team_goals": home_team_goals,
+                "home_team_behinds": home_team_behinds,
                 "away_team_score": away_team_score,
+                "away_team_goals": away_team_goals,
+                "away_team_behinds": away_team_behinds,
                 "match_date": match_date
     }
     return match_information
@@ -97,11 +92,11 @@ def get_relevant_content_from_match(match_url):
 
 
 if __name__ == '__main__':
+    COMPETITION = 1
+    COMP_SEASON = 43
+    GAMEWEEK = 3
+    url = "https://www.afl.com.au/fixture?Competition=%s&CompSeason=%s&MatchTimezone=MY_TIME&Regions=2&GameWeeks=%s&Teams=1&Venues=12" % (COMPETITION,COMP_SEASON, GAMEWEEK)
     s = get_soup_data(url)
-    #for child in s.recursiveChildGenerator():
-    #    if child.name:
-    #        print(child.get('class'))
-
     relevant_links = get_all_links_from_afl_homepage(s)
     for link in relevant_links:
         print(link)
