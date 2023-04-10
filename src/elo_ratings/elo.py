327e7fda99ecd3dc1718b35455503485eb0edf48
James Fricker <jimmyf98@hotmail.com>
2023-04-10 21:12:58 +0930

    Update elo.py

--- src/elo_ratings/elo.py
+++ src/elo_ratings/elo.py
@@ -36,18 +36,33 @@ def update_elo_ratings(
     return home_team_elo, away_team_elo
 
 
+def end_of_season_adjustment(elo_ratings):
+    # at the end of the season
+    # revert elos 1/3 of the way to the base elo
+    base_elo = 1500
+    for team in elo_ratings:
+        elo_ratings[team] = base_elo + (elo_ratings[team] - base_elo) * 2 / 3
+    return elo_ratings
+
+
 def compute_elo_ratings(games):
     elo_ratings = {}
+    season = ""
     for _, game in games.iterrows():
-        home_team = game["HomeTeam"]
-        away_team = game["AwayTeam"]
-        home_team_score = game["HomeScore"]
-        away_team_score = game["AwayScore"]
-
+        home_team = game["home_team_name"]
+        away_team = game["away_team_name"]
+        home_team_score = game["home_team_score"]
+        away_team_score = game["away_team_score"]
         if home_team not in elo_ratings:
             elo_ratings[home_team] = 1500
         if away_team not in elo_ratings:
             elo_ratings[away_team] = 1500
+
+        if game["year"] != season:
+            # end of season, adjust elo ratings
+            elo_ratings = end_of_season_adjustment(elo_ratings)
+
+        season = game["year"]
 
         home_team_elo, away_team_elo = elo_ratings[home_team], elo_ratings[away_team]
         new_home_team_elo, new_away_team_elo = update_elo_ratings(
@@ -62,12 +77,16 @@ def compute_elo_ratings(games):
     return elo_ratings
 
 
-games = pd.read_csv("../scrape_afl/afl.csv")
+games = pd.read_csv("../outputs/afl_data.csv")
 # Sort the DataFrame by year and round
-games = games.sort_values(by=["Year", "Round"], ascending=[True, True])
+games = games.sort_values(by=["year", "round"], ascending=[True, True])
 
 # Reset the index of the DataFrame
 games = games.reset_index(drop=True)
 
 elo_ratings = compute_elo_ratings(games)
+# sort by key
+elo_ratings = {k: v for k, v in sorted(elo_ratings.items(), key=lambda item: item[1])}
+# reverse
+elo_ratings = dict(reversed(list(elo_ratings.items())))
 print(elo_ratings)
